85fe1f138afcb2e837f06f1decfac485747bed84
Kamran <kamran@larasoft.io>
2021-08-12 14:02:21 +0500

    minor

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/__cypress_/csrf_token', function () {
+Route::get('/__cypress__/csrf_token', function () {
     return csrf_token();
 });
 
